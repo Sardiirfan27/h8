c17ec6a1ae1241f61cf84a68eb09cd93cd7ce4ea
Sardi Irfansyah <64241627+Sardiirfan27@users.noreply.github.com>
2024-10-29 06:00:39 +0700

    Update data_preparation_d3.sql

--- kemendag/sql/data_preparation_d3.sql
+++ kemendag/sql/data_preparation_d3.sql
@@ -12,9 +12,9 @@ INSERT INTO sales (order_id, product, category, month, quantity, price_per_unit)
 (2, 'Laptop', 'Electronics', 'January', 3, 1500),
 (3, 'Tablet', 'Electronics', 'January', 5, 300),
 (4, 'Smartphone', 'Electronics', 'February', 4, 800),
-(5, 'Tablet', 'Electronics', 'February', 6, 300),
+(5, 'Table', 'Furniture', 'February', 6, 600),
 (6, 'Laptop', 'Electronics', 'February', 1, 1500),
-(7, 'TV', 'Electronics', 'January', 1, 2000),
+(7, 'Chair', 'Furniture', 'January', 2, 250),
 (8, 'Laptop', 'Electronics', 'March', 2, 1500),
-(9, 'Smartphone', 'Electronics', 'March', 3, 800),
+(9, 'Sofa', 'Furniture', 'March', 1, 1200),
 (10, 'TV', 'Electronics', 'March', 2, 2000);
